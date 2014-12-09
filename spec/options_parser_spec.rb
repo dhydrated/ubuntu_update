1d9c10d31e286c7b56604aab7234906ff73fde9c
taufekj <taufek.johar@my.experian.com>
2014-12-09 16:40:54 +0800

    refactor tests

--- spec/options_parser_spec.rb
+++ spec/options_parser_spec.rb
@@ -10,20 +10,19 @@ describe UbuntuUpdate::OptionsParser, "#get_summary" do
 	# 	expect(options_parser.get_options).not_to include(:verbose)
 	# end
 
+	subject(:options_parser) { options_parser = UbuntuUpdate::OptionsParser.new }
+
 	it "it should contains :verbose when passed -v in the argument" do
-		options_parser = UbuntuUpdate::OptionsParser.new
 		options_parser.parse(["-v"])
 		expect(options_parser.get_options).to include({:verbose=>true})
 	end
 
 	it "it should contains :update when passed -p in the argument" do
-		options_parser = UbuntuUpdate::OptionsParser.new
 		options_parser.parse(["-p"])
 		expect(options_parser.get_options).to include({:update=>true})
 	end
 
 	it "it should contains :upgrade when passed -g in the argument" do
-		options_parser = UbuntuUpdate::OptionsParser.new
 		options_parser.parse(["-g"])
 		expect(options_parser.get_options).to include({:upgrade=>true})
 	end
--- spec/ubuntu_update_spec.rb
+++ spec/ubuntu_update_spec.rb
@@ -6,70 +6,64 @@ require 'ubuntu_update/command_executor'
 
 describe UbuntuUpdate::Updater, "#execute_command" do
 
-	it "it should show summary when --help is passed in" do
-		options_parser = UbuntuUpdate::OptionsParser.new
-		options_parser.expects(:get_options).at_least(1).returns({:help => true})
-		options_parser.expects(:get_summary).at_least(1).returns("My script summary")
+	subject(:updater) do 
+		updater = UbuntuUpdate::Updater.new options_parser, command_executor
+	end
 
-		command_executor = UbuntuUpdate::CommandExecutor.new
+	let(:command_executor) do
+		command_executor = UbuntuUpdate::CommandExecutor.new		
+	end
+
+	let(:options_parser) do
+		options_parser = UbuntuUpdate::OptionsParser.new		
+	end
+
+	it "it should show summary when --help is passed in" do
 		command_executor.expects(:execute).with(includes("My script summary"))
 
-		updater = UbuntuUpdate::Updater.new options_parser, command_executor
+		options_parser.expects(:get_options).at_least(1).returns({:help => true})
+		options_parser.expects(:get_summary).at_least(1).returns("My script summary")
+		
 		updater.execute_command
 	end
 
 	it "it should run apt-get update when no argument passed in" do
-		options_parser = UbuntuUpdate::OptionsParser.new
 		options_parser.expects(:get_options).at_least(1).returns({})
 
-		command_executor = UbuntuUpdate::CommandExecutor.new
 		command_executor.expects(:execute).with("sudo apt-get update")
 
-		updater = UbuntuUpdate::Updater.new options_parser, command_executor
 		updater.execute_command
 	end
 
 	it "it should run apt-get update when passed in {:update=>true}" do
-		options_parser = UbuntuUpdate::OptionsParser.new
 		options_parser.expects(:get_options).at_least(1).returns({:update=>true})
 
-		command_executor = UbuntuUpdate::CommandExecutor.new
 		command_executor.expects(:execute).with("sudo apt-get update")
 
-		updater = UbuntuUpdate::Updater.new options_parser, command_executor
 		updater.execute_command
 	end
 
 	it "it should run apt-get update when passed in {:upgrade=>true}" do
-		options_parser = UbuntuUpdate::OptionsParser.new
 		options_parser.expects(:get_options).at_least(1).returns({:upgrade=>true})
 
-		command_executor = UbuntuUpdate::CommandExecutor.new
 		command_executor.expects(:execute).with("sudo apt-get upgrade")
 
-		updater = UbuntuUpdate::Updater.new options_parser, command_executor
 		updater.execute_command
 	end
 
 	it "it should run apt-get upgrade with -y when passed in {:yes=>true}" do
-		options_parser = UbuntuUpdate::OptionsParser.new
 		options_parser.expects(:get_options).at_least(1).returns({:yes=>true, :upgrade=>true})
 
-		command_executor = UbuntuUpdate::CommandExecutor.new
 		command_executor.expects(:execute).with("sudo apt-get upgrade -y")
 
-		updater = UbuntuUpdate::Updater.new options_parser, command_executor
 		updater.execute_command
 	end
 
 	it "it should run apt-get upgrade and upgrade when passed in {:update=>true, :upgrade=>true}" do
-		options_parser = UbuntuUpdate::OptionsParser.new
 		options_parser.expects(:get_options).at_least(1).returns({:update=>true, :upgrade=>true})
 
-		command_executor = UbuntuUpdate::CommandExecutor.new
 		command_executor.expects(:execute).with("sudo apt-get update && sudo apt-get upgrade")
 
-		updater = UbuntuUpdate::Updater.new options_parser, command_executor
 		updater.execute_command
 	end
 end
